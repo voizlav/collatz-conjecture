14c465ec6fe3ba157087d530e3ba1e386f09541d
Vojislav Trujkic <voizlav@gmail.com>
2022-09-18 09:42:07 +0200

    add collatz.c

---
+++ collatz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int steps = 0;
+
+int colattz(int n)
+{
+    if (n == 1)
+    {
+        steps++;
+        printf("Total steps: %i\n", steps);
+        return n;
+    }
+    else if (n % 2 == 0)
+    {
+        steps++;
+        return colattz(n / 2);
+    } 
+    else
+    {
+        steps++;
+        return colattz(3 * n + 1);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argv[1])
+    {
+        int input = atoi(argv[1]);
+        colattz(input);
+    }
+    else
+    {
+        printf("Usage: colattz [integer]\n\n");
+        return 1;
+    }
+    return 0;
+}
